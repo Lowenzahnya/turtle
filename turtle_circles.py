554ca3544f16a2440d16b183f665bdecde91e251
Oleg <lowenzahnya@yandex.ru>
2023-04-28 13:27:02 +0300

    Цикл кружочков

---
+++ turtle_circles.py
@@ -0,0 +1,16 @@
+import turtle
+import colorsys
+
+turtle.shape('turtle')
+turtle.bgcolor('black')
+turtle.tracer(3, 25)
+size = 0
+h = 0
+
+for i in range(75):
+    c = colorsys.hsv_to_rgb(h, 1, 1)
+    h += 0.1
+    turtle.color(c)
+    turtle.circle(size)
+    turtle.left(5)
+    size = size + 3
